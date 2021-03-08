e782fa4fe2f35f2e80bb0ed0ed38d085993dfbc8
Nikolay Antonovich <TesaloNick@gmail.com>
2021-03-08 21:09:40 +0300

    JS-HW11

--- script.js
+++ script.js
@@ -1,113 +1,124 @@
-const sliderContainer = document.getElementById('image-container')
-const imageRow = document.querySelector('section')
-const mainImageContainer = document.querySelector('.main-img-container')
-const mainImage = document.querySelector('.main-img')
-const pagination = document.querySelector('.pagination')
-const preMainImg = document.querySelector('.pre-main-img')
-const postMainImg = document.querySelector('.post-main-img')
-
-const slides = new Array(10)
-slides.fill('')                     // наполнить массив хоть чем-то, чтобы перебрать массив в последующем
-const arrSlides = [];               // массив со слайдами внизу
-const arrPaginationDot = [];        // массив с пагинацией
 let counter = 0;
-
-slides.forEach((slide, index) => {  // slides.map((slide, index)
-    slide = new Image(230)          // создает картинку с шириной 230
-    slide.src = `images/${index+1}.jpg`
-    slide.style.opacity = '0.5'
-    slide.style.cursor = 'pointer'
-    slide.style.borderRadius = '5px'
-    slide.style.margin = '0 5px'
-    sliderContainer.appendChild(slide)
-    let paginationDot = document.createElement('div')
-    paginationDot.classList.add(`dot${index+1}`)
-    let linkSlideToMain = () => {           // выведение картинки наверх по нажатию на пагинацию и слайд
-        setTimeout(change, 300)
-        counter = index
-        sliderContainer.style.transform = `translate3d(-${index*240}px, 0, 0)`
-        for (let i=0; i < arrPaginationDot.length; i++) {           // перекрасить активные и неактивные слайды и пагинацию
-            if (i === counter) {
-                arrSlides[counter].style.opacity = '1'
-                arrPaginationDot[counter].style.backgroundColor = 'black'
-            } else {
-                arrSlides[i].style.opacity = '0.5'
-                arrPaginationDot[i].style.backgroundColor = 'white'
-            }
+let arrayContacts = [];
+const Contact = function(fullName, age, phone, email) {         // функция-конструктор КОНТАКТОВ
+    this.fullName = fullName;
+    this.age = age;
+    this.phone = phone;
+    this.email = email;
+    this.addContact = function(){
+        counter++
+        arrayContacts.push(`<br>Контакт №${counter}.<br>${this.fullName()}<br>Вам ${this.age} лет.<br>Ваш номер телефона: ${this.phone}.<br>Ваш адрес электронной почты: ${this.email}.<br>`)
+    }
+    this.checkContact = function() {
+        if (this.age < 18) {
+            alert(`Вам нет 18 лет.<br>`)
+        }
+        if (!/(^\+375|^80)( |)(\(|)(33|44|29)(\)|)( |)(\d{7}|\d{3}(-\d{2}){2})/g.test(this.phone)){
+            alert('Вы ввели номер телефона в неправильном формате')
+        }
+        if (!/^[a-zA-Z][a-zA-Z0-9]+\@[\w\.\-]+\.\w{2,11}/g.test(this.email)){
+            alert('Вы ввели адрес электронной почты в неправильном формате')
         }
     }
-    paginationDot.addEventListener('click', linkSlideToMain)    // выведение главной картинки по клику по пагинации
-    slide.addEventListener('click', linkSlideToMain)            // выведение главной картинки по клику по слайду
-    pagination.appendChild(paginationDot)
-    arrSlides.push(slide)
-    arrPaginationDot.push(paginationDot)
-})
-arrSlides[0].style.opacity = '1'
-arrPaginationDot[0].style.backgroundColor = 'black'
-
-const arrowLeft = document.createElement('img')     // создаем стрелки 
-arrowLeft.setAttribute('src', 'images/left.png')
-arrowLeft.classList.add('arrow')
-const arrowRight = document.createElement('img')
-arrowRight.setAttribute('src', 'images/right.png')
-arrowRight.classList.add('arrow')
-
-const drawArrows = () => {                   // рисуем стрелки влево и вправо
-    mainImageContainer.prepend(arrowLeft)
-    mainImageContainer.appendChild(arrowRight)
-}
-drawArrows()
-
-arrowRight.addEventListener('click', slideRight)
-arrowLeft.addEventListener('click', slideLeft)
-let change = function() {                     // выведение картинки на главное изображение
-    if (counter === 0) {
-        preMainImg.setAttribute('src', ``)
-        mainImage.setAttribute('src', `images/${counter+1}.jpg`)
-        postMainImg.setAttribute('src', `images/${counter+2}.jpg`)
-    } else if (counter === slides.length-1){
-        postMainImg.setAttribute('src', ``)
-        mainImage.setAttribute('src', `images/${counter+1}.jpg`)
-        preMainImg.setAttribute('src', `images/${counter}.jpg`)
-    } else {
-        mainImage.setAttribute('src', `images/${counter+1}.jpg`)
-        postMainImg.setAttribute('src', `images/${counter+2}.jpg`)
-        preMainImg.setAttribute('src', `images/${counter}.jpg`) 
+    this.showContact = function() {
+        return document.write(`<br>Контакт №${counter}.<br>${this.fullName()}<br>Вам ${this.age} лет.<br>Ваш номер телефона: ${this.phone}.<br>Ваш адрес электронной почты: ${this.email}.<br>`) 
+    }
+    this.showAllContact = function() {
+        document.write(arrayContacts.join(''))
     }
 }
 
-function slideRight() {         // движение слайдов ВПРАВО по нажатию на стрелку
-    if (counter === slides.length-1) {              // блокировать движение вправо
-        arrowRight.style.opacity = 0.3;
-        arrowRight.style.pointerEvent = 'none';
-        postMainImg.setAttribute('src', ``)
-    } else {
-        ++counter
-        arrowLeft.style.opacity = 1;
-        arrowRight.style.opacity = 1;
-        sliderContainer.style.transform = `translate3d(-${counter*240}px, 0, 0)`  // sliderContainer.style.margin = `-${STEP*300}px`
-        arrSlides[counter].style.opacity = '1'
-        arrSlides[counter-1].style.opacity = '0.5'
-        arrPaginationDot[counter].style.backgroundColor = 'black'
-        arrPaginationDot[counter-1].style.backgroundColor = 'white'
+const Player = function(fullName, age, phone, email, nickname) {        // функция-конструктор ИГРОКОВ
+    Contact.apply(this, arguments)
+    let parentFullName = this.fullName
+    this.fullName = function() {
+        // parentFullName.call(this)
+        return `Вас зовут ${parentFullName}. Вас еще называют: ${nickname}.`
     }
-    setTimeout(change, 300)
+
 }
 
-function slideLeft() {         // движение слайдов ВЛЕВО по нажатию на стрелку
-    if (counter === 0) {
-        arrowLeft.style.opacity = 0.3;
-        arrowLeft.style.pointerEvent = 'none';
-        preMainImg.setAttribute('src', ``)
-    } else {
-        --counter
-        arrowLeft.style.opacity = 1;
-        arrowRight.style.opacity = 1;
-        sliderContainer.style.transform = `translate3d(-${counter*240}px, 0, 0)`
-        arrSlides[counter].style.opacity = '1'
-        arrSlides[counter+1].style.opacity = '0.5'
-        arrPaginationDot[counter].style.backgroundColor = 'black'
-        arrPaginationDot[counter+1].style.backgroundColor = 'white'
+// const jaMorant = new Player('Ja Morant', 21, '+375 (29) 555-44-22', 'jaMorant@gmail.com', 'Boss')
+// jaMorant.addContact()
+// jaMorant.showContact()
+// const johnKonchar = new Player('John Konchar', 24, '+375 (29) 333-44-22', 'johnKonchar@gmail.com', 'Not a Boss')
+// johnKonchar.addContact()
+// johnKonchar.showContact()
+let arrElement = []
+const Element = function() {
+    this.create = function (tagName) {
+        let element = document.createElement(tagName)
+        document.body.appendChild(element)
+        arrElement.push(element)
+        return element
     }
-    setTimeout(change, 300)
+    this.attr = function(element, attrName, attrContent) {
+        element.setAttribute(attrName, attrContent)
+        return attrContent 
+    }
+    this.html = function(element, tagContent) {
+        element.innerHTML = tagContent
+        return tagContent
+    }
+    this.search = function(element, selector) {
+        let searchContent = document.querySelector(selector)
+        return searchContent
+    }
+    this.addClass = function(element, className) {
+        return element.classList.add(className)
+    }
+    this.removeClass = function(element, className) {
+        return element.classList.remove(className)
+    }
+    this.toggleClass = function(element, className) {
+        return element.classList.toggle(className)
+    }
+    this.hasClass = function(element, className) {
+        return element.classList.contains(className)
+    }
+    this.append = function(element, parentTag, beforeOrIn) {
+        if (beforeOrIn === 'beforeElement') {
+            return parentTag.before(element)
+        } if (beforeOrIn === 'inElement' || beforeOrIn == null) {
+            return parentTag.append(element)
+        } else {
+            return alert('вы ввели неправильный третий аргумент "beforeElement"')
+        } 
+    }
+    this.on = function(element, eventName, className) { // проверка на клик по тегу с определенным классом
+        for (let i=0; i< arrElement.length; i++) {
+            if (element === arrElement[i]) {
+                element.addEventListener(eventName, () => {
+                    console.log(`вы совершили действие ${eventName} по элементу c классом ${className}: ${this.hasClass(arrElement[i], className)}`);
+                })
+            }
+        }
+
+
+    }
+}
+const div = new Element()
+const divBig = div.create('div')
+div.attr(divBig, 'style', 'padding: 20px; width: 300px; height: 300px; border: 2px solid black')
+div.addClass(divBig, 'mega-div')
+
+const div1 = new Element()
+const divSmall = div1.create('div')
+div1.attr(divSmall, 'style', 'padding: 20px; width: 100px; height: 100px; border: 2px solid black')
+div1.addClass(divSmall, 'mega-div1')
+
+const p = new Element()
+const text = p.create('p')
+p.attr(text, 'style', 'font-size: 40px; font-weight: 900')
+p.html(text, 'Содержимое тэга')
+p.addClass(text, 'mega-tag')
+p.addClass(text, 'mega-tag1')
+p.removeClass(text, 'mega-tag')
+p.toggleClass(text, 'mega-tag')
+console.log(p.hasClass(text, 'mega-tag'));
+p.append(text, divBig, 'beforeElement') // добавили текст перед блоком
+p.append(divSmall, divBig)      // добавили блок в блок
+for (let i=0; i< arrElement.length; i++) {
+    p.on(arrElement[i], 'click', 'mega-tag')
+
 }
