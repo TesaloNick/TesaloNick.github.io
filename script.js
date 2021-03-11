892c0b9589dea8c02da2db4d20a44a57f4fc4760
Nikolay Antonovich <TesaloNick@gmail.com>
2021-03-11 16:45:40 +0300

    Log-in Sign-up

--- script.js
+++ script.js
@@ -1,124 +1,136 @@
-let counter = 0;
-let arrayContacts = [];
-const Contact = function(fullName, age, phone, email) {         // функция-конструктор КОНТАКТОВ
-    this.fullName = fullName;
-    this.age = age;
-    this.phone = phone;
-    this.email = email;
-    this.addContact = function(){
-        counter++
-        arrayContacts.push(`<br>Контакт №${counter}.<br>${this.fullName()}<br>Вам ${this.age} лет.<br>Ваш номер телефона: ${this.phone}.<br>Ваш адрес электронной почты: ${this.email}.<br>`)
-    }
-    this.checkContact = function() {
-        if (this.age < 18) {
-            alert(`Вам нет 18 лет.<br>`)
-        }
-        if (!/(^\+375|^80)( |)(\(|)(33|44|29)(\)|)( |)(\d{7}|\d{3}(-\d{2}){2})/g.test(this.phone)){
-            alert('Вы ввели номер телефона в неправильном формате')
-        }
-        if (!/^[a-zA-Z][a-zA-Z0-9]+\@[\w\.\-]+\.\w{2,11}/g.test(this.email)){
-            alert('Вы ввели адрес электронной почты в неправильном формате')
-        }
-    }
-    this.showContact = function() {
-        return document.write(`<br>Контакт №${counter}.<br>${this.fullName()}<br>Вам ${this.age} лет.<br>Ваш номер телефона: ${this.phone}.<br>Ваш адрес электронной почты: ${this.email}.<br>`) 
-    }
-    this.showAllContact = function() {
-        document.write(arrayContacts.join(''))
-    }
-}
+const buttonSignUp = document.querySelector('.button-sign-up') // кнопки перехода
+const buttonLogIn = document.querySelector('.button-log-in')
+const buttonMainPage = document.querySelectorAll('.button-main-page')
 
-const Player = function(fullName, age, phone, email, nickname) {        // функция-конструктор ИГРОКОВ
-    Contact.apply(this, arguments)
-    let parentFullName = this.fullName
-    this.fullName = function() {
-        // parentFullName.call(this)
-        return `Вас зовут ${parentFullName}. Вас еще называют: ${nickname}.`
-    }
+const logIn = document.querySelector('.log-in')     // разделы сайта
+const signUp = document.querySelector('.sign-up')
+const mainPage = document.querySelector('.main-page')
 
-}
+const adressRegistration = document.querySelector('.adress-registration')   // поля ввода данных
+const passwordRegistration = document.querySelector('.password-registration')
+const passwordRegistrationRepeat = document.querySelector('.password-registration-repeat')
+const adressLogIn = document.querySelector('.adress-log-in')
+const passwordLogIn = document.querySelector('.password-log-in')
 
-// const jaMorant = new Player('Ja Morant', 21, '+375 (29) 555-44-22', 'jaMorant@gmail.com', 'Boss')
-// jaMorant.addContact()
-// jaMorant.showContact()
-// const johnKonchar = new Player('John Konchar', 24, '+375 (29) 333-44-22', 'johnKonchar@gmail.com', 'Not a Boss')
-// johnKonchar.addContact()
-// johnKonchar.showContact()
-let arrElement = []
-const Element = function() {
-    this.create = function (tagName) {
-        let element = document.createElement(tagName)
-        document.body.appendChild(element)
-        arrElement.push(element)
-        return element
+const sendRegistration = document.querySelector('.send-registration')   // кнопки отправки данных
+const sendLogIn = document.querySelector('.send-log-in')
+
+let wrongInput = document.createElement('p')       // строка об ошибке при неправильном вводе данных
+wrongInput.style.color = 'red'
+wrongInput.classList.add('wrong-input-value')
+
+let arrFormRegistration = []
+const Site = function() {
+    const self = this
+    this.showMainPage = function() {
+        mainPage.style.display = 'block'
+        signUp.style.display = 'none'
+        logIn.style.display = 'none'
+        buttonLogIn.addEventListener('click', self.showAuthPage)
+        buttonSignUp.addEventListener('click', self.showRegistrationPage)
     }
-    this.attr = function(element, attrName, attrContent) {
-        element.setAttribute(attrName, attrContent)
-        return attrContent 
+    this.showRegistrationPage = function() {
+        mainPage.style.display = 'none'
+        signUp.style.display = 'block'
+        logIn.style.display = 'none'
+        buttonMainPage[1].addEventListener('click', self.showMainPage)
+        sendRegistration.addEventListener('click', self.registration)
+        // adressRegistration.value = 'tesa@gmail.com'
+        // passwordRegistration.value = '123456'
+        // passwordRegistrationRepeat.value = '123456'
+        adressRegistration.addEventListener('keydown', (event) => {   // добавляет задачу по нажатию на Enter
+            if (event.key === 'Enter') self.registration()
+        })
+        passwordRegistration.addEventListener('keydown', (event) => {   // добавляет задачу по нажатию на Enter
+            if (event.key === 'Enter') self.registration()
+        })
+        passwordRegistrationRepeat.addEventListener('keydown', (event) => {   // добавляет задачу по нажатию на Enter
+            if (event.key === 'Enter') self.registration()
+        })
+        document.querySelector('.link-to-log-in').addEventListener('click', self.showAuthPage) // переход на страницу входа по ссылке
     }
-    this.html = function(element, tagContent) {
-        element.innerHTML = tagContent
-        return tagContent
+    this.showAuthPage = function() {
+        mainPage.style.display = 'none'
+        signUp.style.display = 'none'
+        logIn.style.display = 'block'
+        buttonMainPage[0].addEventListener('click', self.showMainPage)
+        sendLogIn.addEventListener('click', self.login)
+        adressLogIn.addEventListener('keydown', (event) => {          // добавляет задачу по нажатию на Enter
+            if (event.key === 'Enter') self.login()
+        })
+        passwordLogIn.addEventListener('keydown', (event) => {          // добавляет задачу по нажатию на Enter
+            if (event.key === 'Enter') self.login()
+        })
+        document.querySelector('.link-to-sign-up').addEventListener('click', self.showRegistrationPage) // переход на страницу регистрации по ссылке
     }
-    this.search = function(element, selector) {
-        let searchContent = document.querySelector(selector)
-        return searchContent
-    }
-    this.addClass = function(element, className) {
-        return element.classList.add(className)
-    }
-    this.removeClass = function(element, className) {
-        return element.classList.remove(className)
-    }
-    this.toggleClass = function(element, className) {
-        return element.classList.toggle(className)
-    }
-    this.hasClass = function(element, className) {
-        return element.classList.contains(className)
-    }
-    this.append = function(element, parentTag, beforeOrIn) {
-        if (beforeOrIn === 'beforeElement') {
-            return parentTag.before(element)
-        } if (beforeOrIn === 'inElement' || beforeOrIn == null) {
-            return parentTag.append(element)
+    this.registration = function() {
+        const user = {email: adressRegistration.value, password: passwordRegistration.value, repeatPassword: passwordRegistrationRepeat.value}
+
+        if (passwordRegistration.value !== passwordRegistrationRepeat.value) {  // проверка на правильность ввода пароля
+            wrongInput.innerHTML = 'Второй пароль не совпал с первым' 
+            if (!(signUp.contains(document.querySelector('.wrong-input-value')))) {
+                sendRegistration.insertAdjacentElement('beforebegin', wrongInput)
+            } 
         } else {
-            return alert('вы ввели неправильный третий аргумент "beforeElement"')
-        } 
-    }
-    this.on = function(element, eventName, className) { // проверка на клик по тегу с определенным классом
-        for (let i=0; i< arrElement.length; i++) {
-            if (element === arrElement[i]) {
-                element.addEventListener(eventName, () => {
-                    console.log(`вы совершили действие ${eventName} по элементу c классом ${className}: ${this.hasClass(arrElement[i], className)}`);
-                })
+            if ((adressRegistration.value === '') || (passwordRegistration.value === '') || (passwordRegistrationRepeat.value === '')) {      // проверка на заполнение всех форм
+                wrongInput.innerHTML = 'Заполните все поля формы' 
+                if (!(signUp.contains(document.querySelector('.wrong-input-value')))) {
+                    sendRegistration.insertAdjacentElement('beforebegin', wrongInput)
+                } 
+            } else {
+                if (passwordRegistration.value.split('').length < 6) {   // проверка на длину пароля
+                    wrongInput.innerHTML = 'Пароль должен состоять не менее чем из 6 символов' 
+                    if (!(signUp.contains(document.querySelector('.wrong-input-value')))) {
+                        sendRegistration.insertAdjacentElement('beforebegin', wrongInput)
+                    }
+                } else {
+                    if (!(/^[a-zA-Z][a-zA-Z0-9]+\@[\w\.\-]+\.\w{2,11}/g.test(adressRegistration.value))) { // проверка на правильность ввода email'a
+                        wrongInput.innerHTML = 'Вы неправильно ввели адрес электронной почты' 
+                        if (!(signUp.contains(document.querySelector('.wrong-input-value')))) {
+                            sendRegistration.insertAdjacentElement('beforebegin', wrongInput)
+                        } 
+                    } else {
+                        if (signUp.contains(document.querySelector('.wrong-input-value'))) { // удаляет предупреждающую надпись
+                            sendRegistration.previousElementSibling.remove()
+                        }
+                        arrFormRegistration.push(user)
+                    }
+                }
             }
         }
-
-
+        console.log(arrFormRegistration);
+    }
+    this.login = function() {
+        if (adressLogIn.value === '') {      // проверка на заполнение всех форм
+            wrongInput.innerHTML = 'Заполните все поля формы' 
+            if (!(logIn.contains(document.querySelector('.wrong-input-value')))) {
+                sendLogIn.insertAdjacentElement('beforebegin', wrongInput)
+            } 
+        } else {
+            const user = arrFormRegistration.find(currentUser => currentUser.email === adressLogIn.value) // создает объект с искомыми параметрами 
+            if (user === undefined) {       // проверка на наличие в массиве искомого email'a
+                wrongInput.innerHTML = 'Вы ввели неправильный адрес электронной почты'
+                if (!(logIn.contains(document.querySelector('.wrong-input-value')))) {
+                    sendLogIn.insertAdjacentElement('beforebegin', wrongInput)
+                } 
+            } else {
+                if (user.password !== passwordLogIn.value) {
+                    wrongInput.innerHTML = 'Вы ввели неправильный пароль'
+                    if (!(logIn.contains(document.querySelector('.wrong-input-value')))) {
+                        sendLogIn.insertAdjacentElement('beforebegin', wrongInput)
+                    } 
+                } else {            // проверка на правильность воода массива
+                    if (logIn.contains(document.querySelector('.wrong-input-value'))) { // удаляет предупреждающую надпись
+                        sendLogIn.previousElementSibling.remove()
+                    }
+                    console.log(user);
+                    alert('ПОЗДРАВЛЯЮ. Вы авторизировались!')
+                }
+            } 
+        }
     }
 }
-const div = new Element()
-const divBig = div.create('div')
-div.attr(divBig, 'style', 'padding: 20px; width: 300px; height: 300px; border: 2px solid black')
-div.addClass(divBig, 'mega-div')
 
-const div1 = new Element()
-const divSmall = div1.create('div')
-div1.attr(divSmall, 'style', 'padding: 20px; width: 100px; height: 100px; border: 2px solid black')
-div1.addClass(divSmall, 'mega-div1')
+const site = new Site()
+site.showMainPage()
 
-const p = new Element()
-const text = p.create('p')
-p.attr(text, 'style', 'font-size: 40px; font-weight: 900')
-p.html(text, 'Содержимое тэга')
-p.addClass(text, 'mega-tag')
-p.addClass(text, 'mega-tag1')
-p.removeClass(text, 'mega-tag')
-p.toggleClass(text, 'mega-tag')
-console.log(p.hasClass(text, 'mega-tag'));
-p.append(text, divBig, 'beforeElement') // добавили текст перед блоком
-p.append(divSmall, divBig)      // добавили блок в блок
-for (let i=0; i< arrElement.length; i++) {
-    p.on(arrElement[i], 'click', 'mega-tag')
-
-}
