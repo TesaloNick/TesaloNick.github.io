0840041e12ced1dcd02e39f3e65b4b75ed67a981
Nikolay Antonovich <TesaloNick@gmail.com>
2021-03-18 22:05:30 +0300

    Start

--- script.js
+++ script.js
@@ -39,7 +39,14 @@ class Contacts{
                 } 
             } 
         }
-        if (counter === 0) this.data.push(newUser.data) // добавление контакта в массив this.data
+        if (counter === 0) {
+            console.log();
+            if (event.currentTarget[0].value !== '') { // проверка на пустую строку ID
+                this.data.push(newUser.data)        // добавление контакта в массив this.data
+            } else {
+                alert('Введите ID')
+            }
+        }  
         
         for (let i=0; i < event.currentTarget.length; i++) { 
             event.currentTarget[i].value = ''
@@ -110,98 +117,32 @@ class ContactsApp extends Contacts{
         let div = document.createElement('div')
         div.classList.add('contacts')
         document.body.appendChild(div)
-
-        let formAdd = document.createElement('form') // добавление контакта
-        formAdd.classList.add('form-add')
-        div.appendChild(formAdd)
-        let titleAdd = document.createElement('h1')
-        titleAdd.innerHTML = "Добавить контакт"
-        formAdd.appendChild(titleAdd)
-        let idAdd = document.createElement('input')
-        idAdd.classList.add('id-add')
-        idAdd.setAttribute('type', 'number')
-        formAdd.appendChild(idAdd)
-        let nameAdd = document.createElement('input')
-        nameAdd.classList.add('name-add')
-        nameAdd.setAttribute('type', 'text')
-        formAdd.appendChild(nameAdd)
-        let emailAdd = document.createElement('input')
-        emailAdd.classList.add('email-add')
-        emailAdd.setAttribute('type', 'email')
-        formAdd.appendChild(emailAdd)
-        let adddressAdd = document.createElement('input')
-        adddressAdd.classList.add('address-add')
-        adddressAdd.setAttribute('type', 'text')
-        formAdd.appendChild(adddressAdd)
-        let phoneAdd = document.createElement('input')
-        phoneAdd.classList.add('phone-add')
-        phoneAdd.setAttribute('type', 'tel')
-        formAdd.appendChild(phoneAdd)
-        idAdd.setAttribute('placeholder', 'id')
-        nameAdd.setAttribute('placeholder', 'name')
-        emailAdd.setAttribute('placeholder', 'email')
-        adddressAdd.setAttribute('placeholder', 'adddress')
-        phoneAdd.setAttribute('placeholder', 'phone')
-        let buttonAddContact = document.createElement('button')
-        buttonAddContact.classList.add('button-add-contact')
-        buttonAddContact.innerHTML = "Добавить контакт"
-        formAdd.appendChild(buttonAddContact)
-
-        let formEdit = document.createElement('form') // изменение контакта
-        formEdit.classList.add('form-edit')
-        div.appendChild(formEdit)
-        let titleEdit = document.createElement('h1')
-        titleEdit.innerHTML = "Изменить контакт"
-        formEdit.appendChild(titleEdit)
-        let idEdit = document.createElement('input')
-        idEdit.classList.add('id-edit')
-        formEdit.appendChild(idEdit)
-        let nameEdit = document.createElement('input')
-        nameEdit.classList.add('name-edit')
-        formEdit.appendChild(nameEdit)
-        let emailEdit = document.createElement('input')
-        emailEdit.classList.add('email-edit')
-        formEdit.appendChild(emailEdit)
-        let adddressEdit = document.createElement('input')
-        adddressEdit.classList.add('adddress-edit')
-        formEdit.appendChild(adddressEdit)
-        let phoneEdit = document.createElement('input')
-        phoneEdit.classList.add('phone-edit')
-        formEdit.appendChild(phoneEdit)
-        idEdit.setAttribute('type', 'number')
-        nameEdit.setAttribute('type', 'text')
-        emailEdit.setAttribute('type', 'email')
-        adddressEdit.setAttribute('type', 'text')
-        phoneEdit.setAttribute('type', 'tel')
-        idEdit.setAttribute('placeholder', 'id')
-        nameEdit.setAttribute('placeholder', 'name')
-        emailEdit.setAttribute('placeholder', 'email')
-        adddressEdit.setAttribute('placeholder', 'adddress')
-        phoneEdit.setAttribute('placeholder', 'phone')
-        let buttonEditContact = document.createElement('button')
-        buttonEditContact.classList.add('button-edit-contact')
-        buttonEditContact.innerHTML = "Изменить контакт"
-        formEdit.appendChild(buttonEditContact)
-
-        let formRemove = document.createElement('form') // удаление контакта
-        formRemove.classList.add('form-remove')
-        div.appendChild(formRemove)
-        let titleRemove = document.createElement('h1')
-        titleRemove.innerHTML = "Удалить контакт"
-        formRemove.appendChild(titleRemove)
-        let idRemove = document.createElement('input')
-        idRemove.classList.add('phone-remove')
-        idRemove.setAttribute('type', 'number')
-        idRemove.setAttribute('placeholder', 'id')
-        formRemove.appendChild(idRemove)
-        let buttonRemoveContact = document.createElement('button')
-        buttonRemoveContact.classList.add('button-remove-contact')
-        buttonRemoveContact.innerHTML = "Удалить контакт"
-        formRemove.appendChild(buttonRemoveContact)
-
-        let resultContacts = document.createElement('div') // добавление контейнера с текстом
-        resultContacts.classList.add('result-contacts')
-        document.querySelector('.contacts').appendChild(resultContacts)
+        div.innerHTML = `
+        <form class="form-add">
+            <h1>Добавить контакт</h1>
+            <input type="number" placeholder="id" class="id-add">
+            <input type="text" placeholder="name" class="name-add">
+            <input type="email" placeholder="email" class="email-add">
+            <input type="text" placeholder="address" class="address-add">
+            <input type="tel" placeholder="phone" class="phone-add">
+            <button class="button-add-contact">Добавить контакт</button>
+        </form>
+        <form class="form-edit">
+            <h1>Изменить контакт</h1>
+            <input type="number" placeholder="id" class="id-edit">
+            <input type="text" placeholder="name" class="name-edit">
+            <input type="email" placeholder="email" class="email-edit">
+            <input type="text" placeholder="address" class="address-edit">
+            <input type="tel" placeholder="phone" class="phone-edit">
+            <button class="button-edit-contact">Изменить контакт</button>
+        </form>
+        <form class="form-remove">
+            <h1>Удалить контакт</h1>
+            <input type="tel" placeholder="phone" class="phone-remove">
+            <button class="button-remove-contact">Удалить контакт</button>
+        </form>
+        <div class="result-contacts"></div>
+        `
     }
     onAdd(){        // добавление данных из DOM в массив
         document.querySelector('.form-add').addEventListener('submit', this.add)
