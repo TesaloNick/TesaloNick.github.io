d0ddba4df28be6d7cf309f393fca58a0c6c22db4
Nikolay Antonovich <TesaloNick@gmail.com>
2021-07-22 09:31:05 +0300

    Doob page

---
+++ script.js
@@ -0,0 +1,8 @@
+$(function() {
+    $('.slider').slick({
+        dots: true,
+        speed: 1000,
+        prevArrow: "<img src='./img/left-arrow.svg' class='prev' alt='1'>",
+        nextArrow: "<img src='./img/right-arrow.svg' class='next' alt='2'>"
+    })
+})
