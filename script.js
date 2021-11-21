831bb0d9dc5aa96ae7fa97f78aac65fd872fd8a5
Nikolay Antonovich <TesaloNick@gmail.com>
2021-11-21 22:43:22 +0300

    skinclub1

--- script.js
+++
@@ -1,8 +0,0 @@
-$(function() {
-    $('.slider').slick({
-        dots: true,
-        speed: 1000,
-        prevArrow: "<img src='./img/left-arrow.svg' class='prev' alt='1'>",
-        nextArrow: "<img src='./img/right-arrow.svg' class='next' alt='2'>"
-    })
-})
