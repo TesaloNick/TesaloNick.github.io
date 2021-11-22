34ef4639647d5d2d80669ad15f8398fc9a1721a1
Nikolay Antonovich <TesaloNick@gmail.com>
2021-11-23 00:03:50 +0300

    skinclub2.4

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 document.querySelector('.center-informations__button').addEventListener('click', function (){
-  return console.log('1');
+  document.querySelector('.center-informations__case-page').classList.toggle('no-active')
+  document.querySelector('.center-informations__mask-group-block').classList.toggle('no-active')
 })
+
+let array = document.querySelectorAll('.mask-group__img')
+
+for (let i = 0; i < array.length; i++) {
+  array[i].addEventListener('click', (event) => {
+    console.dir(event.target.src);
+    document.querySelector('.center-informations__item').classList.toggle('no-active')
+    document.querySelector('.center-informations__logo').src = event.target.src
+  })
+}
+// console.log(array);
