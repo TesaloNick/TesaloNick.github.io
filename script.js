b7cab4522a0644b84a0c5f0e084e5c3788802b73
Nikolay Antonovich <TesaloNick@gmail.com>
2021-11-23 16:39:43 +0300

    scinclub2.5

--- script.js
+++ script.js
@@ -1,3 +1,24 @@
+
+$('.language__select').ddslick({
+  width: '92px'
+});
+
+$(document).ready(function() {
+  setInterval (blinke_funk, Math.random()*1300);
+
+  function blinke_funk() { 
+    var blinke_speed = Math.random()*1300;
+    // $(".background__neon-right-blink").fadeIn(blinke_speed).fadeOut(blinke_speed);
+  }
+});
+
+
+$('.mask-group').slick({
+  centerMode: true,
+  // slidesToShow: 9,
+  swipeToSlide: true,
+});
+
 document.querySelector('.center-informations__button').addEventListener('click', function (){
   document.querySelector('.center-informations__case-page').classList.toggle('no-active')
   document.querySelector('.center-informations__mask-group-block').classList.toggle('no-active')
@@ -13,3 +34,5 @@ for (let i = 0; i < array.length; i++) {
   })
 }
 // console.log(array);
+
+
