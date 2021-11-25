1b832848f597385db7d5404c65771b85122cd54c
Nikolay Antonovich <TesaloNick@gmail.com>
2021-11-25 16:48:02 +0300

    scinclub2.7

--- script.js
+++ script.js
@@ -8,12 +8,15 @@ $(document).ready(function() {
 
   function blinke_funk() { 
     var blinke_speed = Math.random()*1300;
+    var blinke_speed1 = Math.random()*300;
     $(".background__neon-right-blink").fadeIn(blinke_speed).fadeOut(blinke_speed);
+    $(".arrow-down").fadeIn(blinke_speed1).fadeOut(blinke_speed1);
+    $(".arrow-up").fadeIn(blinke_speed1).fadeOut(blinke_speed1);
   }
 });
 
 
-$('.mask-group').slick({
+$('.mask-group__container').slick({
   arrows: false,
   centerMode: true,
   centerPadding: '60px',
@@ -44,7 +47,7 @@ $('.mask-group').slick({
   ]
 });
 
-let array = document.querySelectorAll('.mask-group__img')
+let array = document.querySelectorAll('.mask-group img')
 
 for (let i = 0; i < array.length; i++) {
   array[i].addEventListener('click', (event) => {
@@ -54,9 +57,20 @@ for (let i = 0; i < array.length; i++) {
   })
 }
 
-document.querySelector('.center-informations__button').addEventListener('click', function (){
-  document.querySelector('.center-informations__case-page').classList.toggle('no-active')
-  document.querySelector('.center-informations__mask-group-block').classList.toggle('no-active')
-  // document.querySelector('.mask-group').classList.toggle('no-active')
-})
 
+$('.button-yellow').click(function(e) {
+  e.preventDefault();
+  $('.center-informations__case-page').addClass('no-active')
+  $('.center-informations__mask-group-block').removeClass('no-active')
+  $('.button-green').removeClass('no-active')
+  $('.center-informations__goal').removeClass('no-active')
+  $('.background__koko').removeClass('no-active')
+  $('.button-yellow').addClass('no-active')
+  // $('.arrow-down').addClass('no-active')
+  // $('.arrow-up').addClass('no-active')
+  // $('.mask-group').css("opacity", "0.2")
+  let number = Math.round(Math.random()*14)
+  $('.mask-group__container').slick('slickGoTo', number-1)
+  $('.center-informations__goal-img').attr('src', `./img/items/${number}.svg`)
+  console.log(document.querySelector('.mask-group__img'+number));
+});
