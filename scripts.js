faeda7b2360549a5baaf3b74292eaa508851f2a2
Nikolay Antonovich <TesaloNick@gmail.com>
2021-12-01 12:46:46 +0300

    art-beton2

--- scripts.js
+++ scripts.js
@@ -1,9 +1,34 @@
 $('.slider-block__slider').slick({
   dots: true,
-  // appendArrows: $('.your-class-arrow'),
   customPaging: function(slider, i) {
     return '<div class="slider-block__dot"></div>';
   },
   prevArrow: '<button id="prev" type="button" class="slider-block__arrow-left"><img src="./img/slider/arrow-left.svg" alt="arrow-left" class="slider-block__arrow-left-img"></button>',
   nextArrow: '<button id="next" type="button" class="slider-block__arrow-right"><img src="./img/slider/arrow-right.svg" alt="arrow-right" class="slider-block__arrow-right-img"></button>',
 });
+
+if ($(window).width() <= 1150) {
+  $('.slider-of-prices').slick({
+    dots: false,
+    centerMode: true, 
+    focusOnSelect: true,
+    prevArrow: '<button id="prev" type="button" class="slider-block__arrow-left"><img src="./img/slider/arrow-left.svg" alt="arrow-left" class="slider-block__arrow-left-img"></button>',
+    nextArrow: '<button id="next" type="button" class="slider-block__arrow-right"><img src="./img/slider/arrow-right.svg" alt="arrow-right" class="slider-block__arrow-right-img"></button>',
+    slidesToShow: 3,
+    centerPadding: '60px',
+    responsive: [
+      {
+        breakpoint: 830,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      // {
+      //   breakpoint: 590,
+      //   settings: {
+      //     slidesToShow: 1,
+      //   }
+      // }
+    ]
+  }); 
+}
