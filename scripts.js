d74b1218692d5a27cacdc75da7463769fdc4084a
Nikolay Antonovich <TesaloNick@gmail.com>
2021-12-01 22:42:25 +0300

    art-beton2

--- scripts.js
+++ scripts.js
@@ -10,7 +10,6 @@ $('.slider-block__slider').slick({
 if ($(window).width() <= 1150) {
   $('.slider-of-prices__block').slick({
     dots: false,
-    // centerMode: true, 
     focusOnSelect: true,
     prevArrow: '<button id="prev" type="button" class="slider-block__arrow-left"><img src="./img/slider/arrow-left.svg" alt="arrow-left" class="slider-block__arrow-left-img"></button>',
     nextArrow: '<button id="next" type="button" class="slider-block__arrow-right"><img src="./img/slider/arrow-right.svg" alt="arrow-right" class="slider-block__arrow-right-img"></button>',
@@ -32,3 +31,53 @@ if ($(window).width() <= 1150) {
     ]
   }); 
 }
+
+const sidebarButtons = document.querySelectorAll('.sidebar-button')
+
+const ourWorksButton = document.querySelector('.sidebar__our-works-button')
+const pricesButton = document.querySelector('.sidebar__prices-button')
+const mapButton = document.querySelector('.sidebar__map-button')
+const background = document.querySelector('.background')
+const prices = document.querySelector('.prices')
+const footer = document.querySelector('.footer')
+
+function scrollTo(scrollToElement, button) {
+  window.scroll({
+    left: 0,
+    top: scrollToElement.offsetTop,
+    behavior: 'smooth'
+  })
+}
+
+ourWorksButton.style.color = '#F2BF14'
+ourWorksButton.previousSibling.style.backgroundColor = '#F2BF14'
+
+window.addEventListener('scroll', () => {
+  console.log(window.pageYOffset);
+  if (window.pageYOffset+window.innerHeight >= footer.offsetTop) {
+    mapButton.style.color = '#F2BF14'
+    mapButton.previousSibling.style.backgroundColor = '#F2BF14'
+    pricesButton.style.color = '#CCCCCC'
+    pricesButton.previousSibling.style.backgroundColor = '#CCCCCC'
+    ourWorksButton.style.color = '#CCCCCC'
+    ourWorksButton.previousSibling.style.backgroundColor = '#CCCCCC'
+  } else if (window.pageYOffset > 0 && window.pageYOffset+window.innerHeight < footer.offsetTop){
+    pricesButton.style.color = '#F2BF14'
+    pricesButton.previousSibling.style.backgroundColor = '#F2BF14'
+    mapButton.style.color = '#CCCCCC'
+    mapButton.previousSibling.style.backgroundColor = '#CCCCCC'
+    ourWorksButton.style.color = '#CCCCCC'
+    ourWorksButton.previousSibling.style.backgroundColor = '#CCCCCC'
+  } else if (window.pageYOffset === 0){
+    ourWorksButton.style.color = '#F2BF14'
+    ourWorksButton.previousSibling.style.backgroundColor = '#F2BF14'
+    mapButton.style.color = '#CCCCCC'
+    mapButton.previousSibling.style.backgroundColor = '#CCCCCC'
+    pricesButton.style.color = '#CCCCCC'
+    pricesButton.previousSibling.style.backgroundColor = '#CCCCCC'
+  }
+})
+
+ourWorksButton.addEventListener('click', () => scrollTo(background, ourWorksButton))
+pricesButton.addEventListener('click', () => scrollTo(prices, pricesButton))
+mapButton.addEventListener('click', () => scrollTo(footer, mapButton))
